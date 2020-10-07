5b881179fa2b15efdd4b61a070914a555328975b
korks2931 <korks2931@wrdsb.ca>
2020-10-07 15:23:06 +0000

    finished a5q6

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *This program will print out rows of lines of * in the size of the 
+ * @author Sevde
  */
 public class Main {
 
@@ -9,7 +10,24 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a Scanner for user input 
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter a number between 1 and 10");
+    int userNumber = input.nextInt();
+     // make a for loop 
+    for (int count = 1; count <= userNumber; count = count + 1){
+    
+      // make a for loop inside of a loop 
+      for(int countTwo = 2; countTwo <= userNumber; countTwo = countTwo + 1){
+        // print it out to the screen
+      System.out.print ("*");
+      }
+      // print it out to the screen
+      System.out.println("*");
+        
+        
+    }
+
     
   }
 }
